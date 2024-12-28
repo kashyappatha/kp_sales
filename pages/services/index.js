934853d6c10568ae19e45074403387af40f31ae9
kashyap-credsoft <kashyapcredsoft@gmail.com>
2024-12-28 11:51:01 +0530

    made changes

--- pages/services/index.js
+++ pages/services/index.js
@@ -51,6 +51,7 @@ const Services = () => {
         // color={color}
         refresh
       />
+      <Bulb/>
         {/* <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg  px-20 pb-20 pt-8  ">
       <IconCloud iconSlugs={slugs} />
     </div> */}
